29f0365afda0047dee6c385d9d3e4216b1815eab
flfelipelopes <fl.felipelopes288@gmail.com>
2020-05-20 18:18:31 -0300

    Criado corpo da cobrinha e adicionado comentário de referenciamento no arquivo script.js

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box,   
+}
 
+// Criar o background do jogo
 function criarBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box)
 }
 
+// Criar o corpo da cobrinha
+function cirarCobrinha(){
+    for(i = 0; i < snake.length; i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+
 criarBG();
+cirarCobrinha();
